a68fc79e000fc71cd77e7eff9fb6bd1f7f88b3f5
Joseph <jandrade008@gmail.com>
2019-12-18 14:26:10 -0800

    preparing to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Ja222320!",
-    database: "burger"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Ja222320!",
+        database: "burger"
+    });
+}
 
 
 connection.connect(function (err) {
